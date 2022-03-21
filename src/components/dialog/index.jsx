53a9d7a0eb5e2b19db8418ca99401953ffdab736
haha861924 <qq861924@gmail.com>
2022-03-21 23:53:37 +0800

    feat: 🎸 useState dialog

--- src/components/dialog/index.jsx
+++ src/components/dialog/index.jsx
@@ -1,12 +1,23 @@
+import Title from "./Title";
+import Content from "./Content";
+import { useState } from "react";
+import Button from "@/components/Button";
 
+export function Dialog({ onCancel }) {
+  const [dialog, setDialog] = useState({
+    title: "",
+    content: "",
+    confirm: false,
+  });
 
-export function Dialog() {
-  const dialogState = {
-
-  }
   return (
-    <div>
-      <div></div>
+    <div className="min-w-screen dialog-box">
+      <div className="dialog-content relative">
+        <Title message={dialog.title} />
+        <Content message={dialog.content} />
+        <Button text="cancel" onClick={onCancel} />
+        {dialog.confirm && <Button text="delete" onClick={onCancel} />}
+      </div>
     </div>
-  )
-} 
+  );
+}
