1cd9ba0696e76182ed6ffcb9a84ab9b226680238
haha861924 <qq861924@gmail.com>
2022-05-02 02:34:04 +0800

    feat: 🎸 add record's slice

---
+++ src/store/slice/record.js
@@ -0,0 +1,44 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initRecords = [
+  {
+    id: 1,
+    memo: "烤肉聚餐",
+    dollars: 500,
+    label: "eat",
+    createDate: Date.now(),
+  },
+  {
+    id: 2,
+    memo: "肯尼掰掰聚會",
+    dollars: 25000,
+    label: "party",
+    createDate: Date.now(),
+  },
+];
+
+export const recordSlice = createSlice({
+  name: "record",
+  initialState: initRecords,
+  reducers: {
+    getRecord(state) {
+      // get all records
+      // TODO fetch data
+    },
+    addRecord(state, action) {
+      // add new record
+      state.initRecords.push({
+        id: Date.now(),
+        memo: action.payload,
+      });
+    },
+    deleteRecord(state, action) {
+      // delete a record
+      state.slice();
+    },
+  },
+});
+
+export const { getRecord, addRecord, deleteRecord } = recordSlice.actions;
+export const selectAll = (state) => state.recordSlice;
+export default recordSlice.reducer;
