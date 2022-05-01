be5cf78dc3812bfc7efdc8f63919d1536cd66167
haha861924 <qq861924@gmail.com>
2022-05-02 03:19:28 +0800

    feat: 🎸 update icon-rounded component to use dynamic svg file

--- src/components/icon/IconRounded.jsx
+++ src/components/icon/IconRounded.jsx
@@ -1,14 +1,14 @@
 import Img from "@/components/Image";
 import { forwardRef } from "react";
 
-const IconRounded = ({ href }, ref) => {
+const IconRounded = ({ href, svg }, ref) => {
   return (
     <a
       href={href}
       ref={ref}
       className="relative flex w-11 items-center justify-center rounded-full bg-yellow-300 p-2 shadow-md"
     >
-      <Img svg={"example"} width={25} height={25} />
+      <Img svg={svg} width={25} height={25} />
     </a>
   );
 };
