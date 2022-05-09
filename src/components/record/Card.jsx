9f21dca8f39b060ce423c2fcadb7363720f413c1
haha861924 <qq861924@gmail.com>
2022-05-09 17:45:40 +0800

    refactor: 💡 for loop record list

--- src/components/record/Card.jsx
+++ src/components/record/Card.jsx
@@ -1,16 +1,14 @@
-import Router from "@/components/Router";
+import { toCurrency } from "@/utils/helper";
+import RouterLink from "@/components/RouterLink";
 
 export default function Card({ memo, dollars }) {
-  // TODO 金額千分位
-
   return (
-    <div className=" my-10 w-11/12 items-center space-x-5 rounded-lg bg-gray-100 py-10 pl-10 shadow-md">
+    <div className="my-10 w-11/12 items-center space-x-5 rounded-lg bg-gray-100 py-10 pl-10 shadow-md">
       <span className="text-2xl">{memo}</span>
-      <span className="text-lg">${dollars}</span>
 
       <div className="flex justify-center space-x-2">
-        <Router href="/" svg="pencil" />
-        <Router href="/about" svg="down" />
+        <RouterLink href="/" svg="pencil" />
+        <RouterLink href="/about" svg="down" />
       </div>
     </div>
   );
